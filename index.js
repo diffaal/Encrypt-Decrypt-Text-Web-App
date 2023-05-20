cf8c1772b30ac042ef18aed9b6fd924ca82efc78
Muhammad Diffa Ananda Lukman <diffa.ananda.10@gmail.com>
2023-05-20 17:04:58 +0700

    remove not required dependency

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
 const fileUpload = require('express-fileupload');
-const cors = require('cors');
 const path = require('path');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.urlencoded({extended: false}));
 app.use(fileUpload());
-app.use(cors({origin: '*'}));
 app.set('view engine', 'ejs');
 
 app.use('/', require('./routes/pages'));
